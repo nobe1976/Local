3897fce2514da107ff0501b47a84d4398914bb43
no1uknow <nobe1976@users.noreply.github.com>
2016-04-21 21:00:36 -0400

    Create logs instead of terminal output
    Seems to not be such resource hog when doing this to ones PC. 
    Terminal read outs all get logged onto a file which can be reviewed for later to fixed minor build issues.

--- Build.sh
+++ Build.sh
@@ -1,46 +1,48 @@
 #!/bin/bash
+rm -rf Zips
+rm -rf Zip_logs
 
 mkdir Zips
-
+mkdir Zip_logs
 make clobber
 
-. build/envsetup.sh && time brunch vs980
+. build/envsetup.sh && time brunch vs980 >Zip_logs/vs980.log 2>&1
 
 mv out/target/product/vs980/DU_vs980_6.0.1_*.zip Zips/DU_vs980_6.0.1_$(date +%Y%m%d).v10.1-DIRTY-DEEDS.zip
 
 make installclean
 
-. build/envsetup.sh && time brunch ls980
+. build/envsetup.sh && time brunch ls980 >Zip_logs/ls980.log 2>&1
 
 mv out/target/product/ls980/DU_ls980_6.0.1_*.zip Zips/DU_ls980_6.0.1_$(date +%Y%m%d).v10.1-DIRTY-DEEDS.zip
 
 make installclean
 
-. build/envsetup.sh && time brunch f320
+. build/envsetup.sh && time brunch f320 >Zip_logs/f320.log 2>&1
 
 mv out/target/product/f320/DU_f320_6.0.1_*.zip Zips/DU_f320_6.0.1_$(date +%Y%m%d).v10.1-DIRTY-DEEDS.zip
 
 make installclean
 
-. build/envsetup.sh && time brunch d803
+. build/envsetup.sh && time brunch d803 >Zip_logs/d803.log 2>&1
 
 mv out/target/product/d803/DU_d803_6.0.1_*.zip Zips/DU_d803_6.0.1_$(date +%Y%m%d).v10.1-DIRTY-DEEDS.zip
 
 make installclean
 
-. build/envsetup.sh && time brunch d802
+. build/envsetup.sh && time brunch d802 >Zip_logs/d802.log 2>&1
 
 mv out/target/product/d802/DU_d802_6.0.1_*.zip Zips/DU_d802_6.0.1_$(date +%Y%m%d).v10.1-DIRTY-DEEDS.zip
 
 make installclean
 
-. build/envsetup.sh && time brunch d801
+. build/envsetup.sh && time brunch d801 >Zip_logs/d801.log 2>&1
 
 mv out/target/product/d801/DU_d801_6.0.1_*.zip Zips/DU_d801_6.0.1_$(date +%Y%m%d).v10.1-DIRTY-DEEDS.zip
 
 make installclean
 
-. build/envsetup.sh && time brunch d800
+. build/envsetup.sh && time brunch d800 >Zip_logs/d800.log 2>&1
 
 mv out/target/product/d800/DU_d800_6.0.1_*.zip Zips/DU_d800_6.0.1_$(date +%Y%m%d).v10.1-DIRTY-DEEDS.zip
 
